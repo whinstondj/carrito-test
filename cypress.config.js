dfdcd8e1a50046355d3e7f0d0f8bc9bd0eb40408
winstoncastillo <whinstondj@gmail.com>
2025-02-20 19:11:45 -0500

    updated record key

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "hema2c",
-  recordKey: '3e1c083f-437d-46da-aab0-7aba7007982a',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
