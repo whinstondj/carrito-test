7e23cb167dc46a6dc8bf93037b2ca3ba963054c2
winstoncastillo <whinstondj@gmail.com>
2025-02-20 19:03:08 -0500

    verificar titulos

--- cypress/e2e/anadir_carrito.cy.js
+++ cypress/e2e/anadir_carrito.cy.js
@@ -5,5 +5,6 @@ describe('ver un producto', () => {
     CarritoActions.visitarCarrito()
     CarritoActions.clickProducto(':nth-child(1) > .product-thumb > .content > .description > h4 > a')
     CarritoActions.revisarTitulo('h1', 'MacBook')
+    CarritoActions.revisarTitulo('#content > :nth-child(1) > :nth-child(2) > :nth-child(2) > :nth-child(1)', 'Brand: Apple')
   })
 })
