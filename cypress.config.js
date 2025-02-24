2ab99ab5efef6b18fcc6487a6dbd86833362bbeb
winstoncastillo <whinstondj@gmail.com>
2025-02-24 17:24:29 -0500

    Configurando env

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "hema2c",
   e2e: {
+  //baseUrl: process.env.CYPRESS_BASE_URL,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  env: {
+    username: process.env.CYPRESS_USERNAME,
+    password: process.env.CYPRESS_PASSWORD,
+  }
 });
